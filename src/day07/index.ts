05e998185c47529cd8ad9a83de8492fa07ed1fa1
Michal Dyczkowski <themich4@gmail.com>
2023-12-08 15:49:14 +0100

    Day 7 - part 1

--- src/day07/index.ts
+++ src/day07/index.ts
@@ -1,20 +1,32 @@
 import { getInput } from "../get-input";
 
 const strength = [
-  "A",
-  "K",
-  "Q",
-  "J",
-  "T",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
   "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "T",
+  "J",
+  "Q",
+  "K",
+  "A",
 ];
+
+const getValue = (count: Record<string, number>) => {
+  const differentCards = Object.values(count).length;
+
+  if (differentCards === 5) return 1;
+  if (differentCards === 4) return 2;
+  if (Object.values(count).filter((v) => v === 2).length === 2) return 3;
+  if (differentCards === 3) return 4;
+  if (Object.values(count).filter((v) => v === 2).length === 1) return 5;
+  if (differentCards === 2) return 6;
+  return 7;
+};
 
 const day07 = async () => {
   const lines = (await getInput("src/day07/input.txt")).split("\n");
@@ -23,44 +35,47 @@ const day07 = async () => {
 
   for (const line of lines) {
     const [cards, bid] = line.split(/\s+/);
-    console.log(cards, bid);
 
     const count = cards.split("").reduce((acc, card) => {
       acc[card] = acc[card] ? acc[card] + 1 : 1;
       return acc;
     }, {});
 
-    const max = Math.max(...Object.values(count));
-
     mapped.push({
       cards,
       bid,
-      max,
+      value: getValue(count),
     });
-
-    console.log({ mapped });
-
-    console.log("=====");
   }
 
   const ordered = mapped.sort((a, b) => {
-    if (a.max > b.max) {
-      console.log(">", a.max, b.max);
+    if (a.value > b.value) {
       return 1;
     }
 
-    if (a.max < b.max) {
-      console.log("<", a.max, b.max);
+    if (a.value < b.value) {
+      return -1;
+    }
+
+    for (let i = 0; i < a.cards.length; i++) {
+      const aIndex = strength.indexOf(a.cards[i]);
+      const bIndex = strength.indexOf(b.cards[i]);
+
+      if (aIndex === bIndex) {
+        continue;
+      }
+
+      if (aIndex > bIndex) {
+        return 1;
+      }
+
       return -1;
     }
 
     return 0;
   });
 
-  console.log({ ordered });
-
-  return ordered.reduce((acc, { bid, max, cards }, index) => {
-    console.log(acc, bid, max, cards, index);
+  return ordered.reduce((acc, { bid, value, cards }, index) => {
     return (acc += bid * (index + 1));
   }, 0);
 };
