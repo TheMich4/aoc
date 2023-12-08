97768aa11762ec47f523b109fcbfd5ea7d2dedb0
Michal Dyczkowski <themich4@gmail.com>
2023-12-08 17:22:52 +0100

    Day 8 - part 2

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -5,6 +5,17 @@ const moveToIndex = {
   R: 1,
 };
 
+function gcd(a: number, b: number) {
+  var t = 0;
+  a < b && ((t = b), (b = a), (a = t)); // swap them if a < b
+  t = a % b;
+  return t ? gcd(b, t) : b;
+}
+
+function lcm(a: number, b: number) {
+  return (a / gcd(a, b)) * b;
+}
+
 const day08 = async () => {
   const [moves, _, ...codes] = (await getInput("src/day08/input.txt")).split(
     "\n"
@@ -23,18 +34,30 @@ const day08 = async () => {
 
   console.log(locationToMoves);
 
-  let location = "AAA";
-  let steps = 0;
+  let currentLocations = Object.keys(locationToMoves).filter(
+    (l) => l[2] === "A"
+  );
+  let steps = [];
 
-  while (true) {
-    for (const move of moves) {
-      console.log(location, move, steps);
-      const moveIndex = moveToIndex[move];
-      location = locationToMoves[location][moveIndex];
-      steps++;
+  console.log(currentLocations, steps);
+
+  currentLocations.forEach((location) => {
+    let currentLocation = location;
+    let currentSteps = 0;
+    while (true) {
+      for (const move of moves) {
+        const moveIndex = moveToIndex[move];
+        currentLocation = locationToMoves[currentLocation][moveIndex];
+        currentSteps++;
+      }
+      if (currentLocation[2] === "Z") {
+        steps.push(currentSteps);
+        break;
+      }
     }
-    if (location === "ZZZ") return steps;
-  }
+  });
+
+  return steps.reduce(lcm);
 };
 
 console.log(await day08());
