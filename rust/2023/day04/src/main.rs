d353e3584c4df2274b1fb3f6b81445fa5a0805c5
Michal Dyczkowski <themich4@gmail.com>
2024-05-14 16:46:47 +0200

    Rust 2023-04-02

--- rust/2023/day04/src/main.rs
+++ rust/2023/day04/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let contents =
         std::fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
@@ -24,23 +26,45 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let mut sum = 0;
+    // let mut sum = 0;
+    //
+    // cards.iter().for_each(|(winning, player)| {
+    //     let mut score = 0;
+    //
+    //     player.iter().for_each(|player_number| {
+    //         if winning.contains(player_number) {
+    //             if score == 0 {
+    //                 score = 1;
+    //             } else {
+    //                 score *= 2;
+    //             }
+    //         }
+    //     });
+    //
+    //     sum += score;
+    // });
+    //
+    // println!("Sum: {}", sum);
 
-    cards.iter().for_each(|(winning, player)| {
-        let mut score = 0;
+    let mut wins: HashMap<i32, i32> = HashMap::new();
+    let mut copies: Vec<i32> = vec![];
 
-        player.iter().for_each(|player_number| {
-            if winning.contains(player_number) {
-                if score == 0 {
-                    score = 1;
-                } else {
-                    score *= 2;
-                }
-            }
-        });
+    cards.iter().enumerate().for_each(|(i, (winning, player))| {
+        let won = player.iter().filter(|x| winning.contains(x)).count();
 
-        sum += score;
+        wins.insert(i as i32, won as i32);
+        copies.push(1 as i32);
     });
 
-    println!("Sum: {}", sum);
+    for i in 0..cards.len() {
+        let win_count = wins.get(&(i as i32)).unwrap();
+
+        for j in (i + 1)..((i + 1) + (*win_count as usize)) {
+            copies[j] += copies[i];
+        }
+    }
+
+    let sum = copies.iter().sum::<i32>();
+
+    println!("Sum: {:?}", sum);
 }
